f33b8a404cb98d986fb3e0da4d864c74ba3f9bde
Mostafa Abdelfatah Elwishy <mostafaelweshy777@gmail.com>
2024-12-19 13:43:50 +0200

    added Laptop model

---
+++ models/Laptop.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const laptopSchema = new mongoose.Schema({
+    name:{
+      type:String,
+      require:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    photo:{
+        type:String,
+
+    },
+    stock:{
+        type:Number,
+        require: true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    Date_created: {
+        type: Date,
+        default: Date.now,
+    },
+    Date_updated: {
+        type: Date,
+        default: Date.now,
+    },
+});
+laptopSchema.pre('findOneAndUpdate', function (next) {
+    this.set({ Date_updated: new Date() });
+    next();
+});
+
+laptopSchema.pre('updateOne', function (next) {
+    this.set({ Date_updated: new Date() });
+    next();
+});
+
+
+
+module.exports = mongoose.model('Laptop', laptopSchema);
